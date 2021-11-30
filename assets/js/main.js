2f4bcc860f934e4f2bcdd04ca2946bc233f047e0
Ariful Islam <arifulislam161024@gmail.com>
2021-11-30 19:26:16 +0600

    reviews the design

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,56 +1,61 @@
 // change navbar color on scroll
-const navbar = document.querySelector('.nav-changed');
+const navbar = document.querySelector(".nav-changed");
 window.onscroll = () => {
-    if (window.scrollY > 90) {
-        navbar.classList.add('nav-active');
-    } else {
-        navbar.classList.remove('nav-active');
-    }
+  if (window.scrollY > 90) {
+    navbar.classList.add("nav-active");
+  } else {
+    navbar.classList.remove("nav-active");
+  }
 };
 //*Our techer in about page
-$(document).ready(function() {
-	$('.testimonial').owlCarousel({
-		loop: true,
-		margin: 10,
-		nav: false,
-		navText: [
-			'<i class="fa fa-chevron-left" aria-hidden="true"></i>',
-			'<i class="fa fa-chevron-right" aria-hidden="true"></i>'
-		],
-		dots: false,
-		responsive: {
-			0: {
-				items: 1
-			},
-			600: {
-				items: 2
-			},
-			1000: {
-				items: 3
-			}
-		}
-	});
+$(document).ready(function () {
+  $(".testimonial").owlCarousel({
+    loop: true,
+    autoplay: true,
+    margin: 10,
+    nav: false,
+    navText: [
+      '<i class="fa fa-chevron-left" aria-hidden="true"></i>',
+      '<i class="fa fa-chevron-right" aria-hidden="true"></i>',
+    ],
+    dots: false,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 2,
+      },
+      1000: {
+        items: 3,
+      },
+    },
+  });
+});
 
-	
-	});
-	
+$(document).ready(function () {
+  $(".popup-gallery").magnificPopup({
+    delegate: "a",
+    type: "image",
+    tLoading: "Loading image #%curr%...",
+    mainClass: "mfp-img-mobile",
+    gallery: {
+      enabled: true,
+      navigateByImgClick: true,
+      preload: [0, 1], // Will preload 0 - before current, and 1 after the current image
+    },
+    image: {
+      tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+      titleSrc: function (item) {
+        return item.el.attr("title");
+      },
+    },
+  });
+});
 
-$(document).ready(function() {
-	$('.popup-gallery').magnificPopup({
-		delegate: 'a',
-		type: 'image',
-		tLoading: 'Loading image #%curr%...',
-		mainClass: 'mfp-img-mobile',
-		gallery: {
-			enabled: true,
-			navigateByImgClick: true,
-			preload: [ 0, 1 ] // Will preload 0 - before current, and 1 after the current image
-		},
-		image: {
-			tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
-			titleSrc: function(item) {
-				return item.el.attr('title');
-			}
-		}
-	});
+let masonry2 = new MiniMasonry({
+  container: document.querySelector(".container_2"),
+  surroundingGutter: false,
+  gutterX: 40,
+  gutterY: 10,
 });
